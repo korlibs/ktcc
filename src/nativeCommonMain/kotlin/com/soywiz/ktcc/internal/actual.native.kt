00b43b4cd85267b89dffa46a5188d24df035f5df
soywiz <soywiz@gmail.com>
2019-01-19 10:57:48 +0100

    Fix windows build

--- src/nativeCommonMain/kotlin/com/soywiz/ktcc/internal/actual.native.kt
+++ src/nativeCommonMain/kotlin/com/soywiz/ktcc/internal/actual.native.kt
@@ -9,9 +9,9 @@ import platform.posix.*
 actual fun readFile(name: String): ByteArray? {
     val fd = fopen(name, "rb")
     return if (fd != null) {
-        fseek(fd, 0L, SEEK_END)
+        fseek(fd, 0L.convert(), SEEK_END)
         val length = ftell(fd)
-        fseek(fd, 0L, SEEK_SET)
+        fseek(fd, 0L.convert(), SEEK_SET)
         val out = ByteArray(length.toInt())
         val read = if (out.isNotEmpty()) {
             out.usePinned { ptr ->
